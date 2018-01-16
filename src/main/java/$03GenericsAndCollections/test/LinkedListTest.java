bad9cc69a13efa5c8025149ca694261a6cfc4556
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2018-01-16 17:17:19 +0300

    LinkedList test as FIFO and LIFO

---
+++ src/main/java/$03GenericsAndCollections/test/LinkedListTest.java
@@ -0,0 +1,45 @@
+package $03GenericsAndCollections.test;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class LinkedListTest {
+    public static void main(String[] args) {
+        // FIFO
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(1);
+        queue.add(2);
+        queue.offer(3);
+        queue.offer(4);
+        System.out.println(queue); // [1, 2, 3, 4]
+        // queue.push(5); no push method
+
+        System.out.println(queue.peek()); // 1 shows current element
+
+        System.out.println(queue.poll()); // 1
+        System.out.println(queue.poll()); // 2
+
+        System.out.println(queue.peek()); // 3
+
+        System.out.println(" ================== ");
+
+        // LIFO
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        linkedList.add(1);
+        linkedList.add(2);
+        System.out.println(linkedList); // [1, 2]
+        linkedList.offer(3);
+        linkedList.offer(4);
+        System.out.println(linkedList); // [1, 2, 3, 4]
+        linkedList.push(5);
+        linkedList.push(6);
+        System.out.println(linkedList); // [6, 5, 1, 2, 3, 4]
+
+        System.out.println(linkedList.peek()); // 6
+
+        System.out.println(linkedList.poll()); // 6
+        System.out.println(linkedList.poll()); // 5
+
+        System.out.println(linkedList.peek()); // 1
+    }
+}
