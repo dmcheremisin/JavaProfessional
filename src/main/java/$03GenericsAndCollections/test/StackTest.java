3eccc72af6122e1f11b121c6c48b95cd9b83a6d2
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2018-01-16 16:58:39 +0300

    Stack structure test

---
+++ src/main/java/$03GenericsAndCollections/test/StackTest.java
@@ -0,0 +1,24 @@
+package $03GenericsAndCollections.test;
+
+import java.util.Stack;
+
+public class StackTest {
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack<>();
+        stack.add("1");
+        stack.add("2");
+        System.out.println(stack); // [1, 2]
+        // push works the same way as add(E)
+        stack.push("3");
+        stack.push("4");
+        System.out.println(stack);// [1, 2, 3, 4]
+        System.out.println(stack.peek()); // 4
+        System.out.println(stack.peek()); // 4
+        System.out.println(stack.pop());  // 4
+        System.out.println(stack); // [1, 2, 3]
+
+        stack.add(0, "5");
+        System.out.println(stack); // [5, 1, 2, 3]
+        System.out.println(stack.get(1)); // 1
+    }
+}
