d34594a0287a57c3e336ab1c0aa56552eca8a5f7
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2018-01-29 15:41:56 +0300

    Collection methods

---
+++ src/main/java/$03GenericsAndCollections/newFeatures/CollectionMethods.java
@@ -0,0 +1,29 @@
+package $03GenericsAndCollections.newFeatures;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Dmitrii on 29.01.2018.
+ */
+public class CollectionMethods {
+    public static void main(String[] args) {
+        // removeIf example
+        List<String> animals = new ArrayList<>();
+        animals.add("Cat");
+        animals.add("Dog");
+        animals.add("Tiger");
+        animals.removeIf(animal -> animal.startsWith("D"));
+        System.out.println(animals);
+
+        // replaceAll example
+        List<Integer> numbers = Arrays.asList(1, 2, 3);
+        numbers.replaceAll(i -> i*2);
+        System.out.println(numbers);
+
+        // foreach example
+        animals.forEach(s -> System.out.println(s));
+        animals.forEach(System.out::println);
+    }
+}
