e857a8bbeca5c1ff1e5f587b2fd22d62902651ab
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2018-01-30 23:52:31 +0300

    Map merge refactor

--- src/main/java/$03GenericsAndCollections/newFeatures/MapMerge.java
+++ src/main/java/$03GenericsAndCollections/newFeatures/MapMerge.java
@@ -15,29 +15,31 @@ public class MapMerge {
         trip.put("Jenny", "Bus tour");
         trip.put("Tom", "Tram");
 
-        String merge1 = trip.merge("Jenny", "Skyride", mapper);// Bus tour > Skyride -> Bus tour
-        System.out.println(merge1); // Bus tour
-        String merge2 = trip.merge("Tom", "Skyride", mapper);// Tram < Skyride -> Skyride
-        System.out.println(merge2); // Skyride
+        trip.merge("Jenny", "Skyride", mapper);// Bus tour > Skyride -> Bus tour
+        trip.merge("Tom", "Skyride", mapper);// Tram < Skyride -> Skyride
         System.out.println(trip);
         // {Tom=Skyride, Jenny=Bus tour}
 
         // case with null value
         trip.put("Sam", null);
-        String merge3 = trip.merge("Sam", "Train", mapper);// mapper function is not clled
-        System.out.println(merge3); // Train
+        trip.merge("Sam", "Train", mapper);// Train - mapper function is not clled
         System.out.println(trip);
         // {Tom=Skyride, Jenny=Bus tour, Sam=Train}
 
         // case mapper returns null value
         BiFunction<String, String, String> nullMapper = (v1, v2) -> null;
-        String sam = trip.merge("Sam", "Tuktuk", nullMapper);
-        System.out.println(sam); // null
+
+        trip.merge("Sam", "Tuktuk", nullMapper);// null
         System.out.println(trip);
         // {Tom=Skyride, Jenny=Bus tour}
+
         trip.merge("Jill", "Car", nullMapper); // mapper is not called
         System.out.println(trip);
         //{Tom=Skyride, Jill=Car, Jenny=Bus tour}
 
+        trip.put("Mike", null);
+        trip.merge("Mike", "Skate", nullMapper); // mapper is not called
+        System.out.println(trip);
+        // {Mike=Skate, Tom=Skyride, Jill=Car, Jenny=Bus tour}
     }
 }
