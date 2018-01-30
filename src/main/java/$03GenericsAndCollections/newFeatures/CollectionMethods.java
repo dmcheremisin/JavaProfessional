9c66fe11e69c59b1eea8fbf6982085a0dbde520d
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2018-01-30 13:31:12 +0300

    Collection methods output examples

--- src/main/java/$03GenericsAndCollections/newFeatures/CollectionMethods.java
+++ src/main/java/$03GenericsAndCollections/newFeatures/CollectionMethods.java
@@ -16,14 +16,18 @@ public class CollectionMethods {
         animals.add("Tiger");
         animals.removeIf(animal -> animal.startsWith("D"));
         System.out.println(animals);
+        // [Cat, Tiger]
 
         // replaceAll example
         List<Integer> numbers = Arrays.asList(1, 2, 3);
         numbers.replaceAll(i -> i*2);
         System.out.println(numbers);
+        // [2, 4, 6]
 
         // foreach example
         animals.forEach(s -> System.out.println(s));
         animals.forEach(System.out::println);
+        // Cat
+        // Tiger
     }
 }
