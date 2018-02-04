9b84b17c9c5318da22d1b5e6d98c8b61445bc51c
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2018-02-04 15:21:39 +0300

    instance and static variables in lambda

--- src/main/java/$04FunctionalInterfaces/common/EffectivelyFinal.java
+++ src/main/java/$04FunctionalInterfaces/common/EffectivelyFinal.java
@@ -7,15 +7,18 @@ interface Gorilla{
     String move();
 }
 class GorillaImpl {
+    static String drive = "fast";
     String walk = "walk";
     void everyOnePlay(boolean baby) {
         String approach = "amble";
         // approach = "run"; -> variable is not effectively final
         // baby = false; variable is not effectively final
-        walk = "fast walk";
+        walk = "fast walk"; // instance variable is normal to reassign
+        drive = "slow"; // static variable is normal to reassign
         play(() -> approach);
         play(() -> baby ? "ride" : "run");
         play(() -> walk);
+        play(() -> drive);
     }
     void play(Gorilla g) {
         System.out.println(g.move());
@@ -28,5 +31,6 @@ public class EffectivelyFinal {
 //        amble
 //        ride
 //        fast walk
+//        slow
     }
 }
