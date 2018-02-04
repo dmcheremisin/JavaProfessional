d74dd793983e3cc210ac739cbb70855c9bd19f80
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2018-02-04 15:37:57 +0300

    Supplier example

---
+++ src/main/java/$04FunctionalInterfaces/builtInFuntionalInterfaces/SupplierExample.java
@@ -0,0 +1,42 @@
+package $04FunctionalInterfaces.builtInFuntionalInterfaces;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.function.Supplier;
+
+/**
+ * Created by Dmitrii on 04.02.2018.
+ */
+/*@FunctionalInterface
+public interface Supplier<T> {
+
+    *//**
+     * Gets a result.
+     *
+     * @return a result
+     *//*
+    T get();
+}*/
+public class SupplierExample {
+    public static void main(String[] args) {
+        Supplier<LocalDate> d1 = () -> LocalDate.now();
+        Supplier<LocalDate> d2 = LocalDate::now;
+
+        System.out.println(d1.get());
+        System.out.println(d2.get());
+        // 2018-02-04
+        // 2018-02-04
+
+        Supplier<ArrayList<String>> list1 = () -> new ArrayList<String>();
+        Supplier<ArrayList<String>> list2 = ArrayList<String>::new;
+        Supplier<ArrayList<String>> list3 = ArrayList::new;
+
+        ArrayList<String> arrayList = list3.get();
+        arrayList.add("First str");
+        arrayList.add("Second str");
+        System.out.println(arrayList);
+        // [First str, Second str]
+        System.out.println(list2);
+        // $04FunctionalInterfaces.builtInFuntionalInterfaces.SupplierExample$$Lambda$7/1791741888@3feba861
+    }
+}
