24c60d54f83a6781ef6aac458554e4474df1bf11
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2018-02-06 15:16:06 +0300

    findFirst and findAny

---
+++ src/main/java/$04FunctionalInterfaces/streams/FindAnyFindFirst.java
@@ -0,0 +1,23 @@
+package $04FunctionalInterfaces.streams;
+
+import java.util.Optional;
+import java.util.stream.Stream;
+
+/**
+ * Created by Dmitrii on 06.02.2018.
+ */
+public class FindAnyFindFirst {
+    public static void main(String[] args) {
+        Stream<String> finite = Stream.of("gorilla", "monkey", "ape");
+        finite.findAny().ifPresent(System.out::println); // gorilla
+
+        Stream<String> infinite = Stream.generate(() -> "chimp");
+        infinite.findAny().ifPresent(System.out::println); // chimp
+
+        Stream<String> parallelAny = Stream.of("gorilla", "monkey", "ape").parallel();
+        Stream<String> parallelFirst = Stream.of("gorilla", "monkey", "ape").parallel();
+        parallelAny.findAny().ifPresent(System.out::println); // monkey
+        parallelFirst.findFirst().ifPresent(System.out::println); // gorilla
+
+    }
+}
