530ed99b7226bc97687c192b6182efb60b388b9b
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2018-03-01 11:02:11 +0300

    abs and relative paths

--- src/main/java/$08InputOutput/filesAndDirectories/FileSample.java
+++ src/main/java/$08InputOutput/filesAndDirectories/FileSample.java
@@ -1,13 +1,20 @@
 package $08InputOutput.filesAndDirectories;
 
 import java.io.File;
+import java.nio.file.Paths;
 
 /**
  * Created by Dmitrii on 01.03.2018.
  */
 public class FileSample {
     public static void main(String[] args) {
-        File file = new File("C:\\Workspace\\JavaProfessional\\src\\main\\resources\\sample.txt");
-        System.out.println(file.exists());//true
+        // get current directory -> C:\Workspace\JavaProfessional
+        System.out.println(Paths.get(".").toAbsolutePath().normalize().toString());
+
+        File fileAbsPath = new File("C:\\Workspace\\JavaProfessional\\src\\main\\resources\\sample.txt");
+        System.out.println(fileAbsPath.exists()); //true
+
+        File fileRelPath = new File("src\\main\\resources\\sample.txt");
+        System.out.println(fileRelPath.exists()); // true
     }
 }
