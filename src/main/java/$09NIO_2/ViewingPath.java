7edf0dbb908e390d0d168b1ae6f03655b3a2a38b
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2018-03-14 19:49:59 +0300

    Viewing the Path

---
+++ src/main/java/$09NIO_2/ViewingPath.java
@@ -0,0 +1,22 @@
+package $09NIO_2;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * Created by Dmitrii on 14.03.2018.
+ */
+public class ViewingPath {
+    public static void main(String[] args) {
+        Path path = Paths.get("some/root/directory/file.name");
+        System.out.println("The path is: " + path.toString());
+        for(int i = 0; i < path.getNameCount(); i++){
+            System.out.println(path.getName(i).toString());
+        }
+        // The path is: some\root\directory\file.name
+        // some
+        // root
+        // directory
+        // file.name
+    }
+}
