2bff70f5fa4e92304ae8e37ee69a4ae753992ce7
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2018-03-14 20:27:30 +0300

    Checking path types

---
+++ src/main/java/$09NIO_2/pathMethods/CheckPathType.java
@@ -0,0 +1,23 @@
+package $09NIO_2.pathMethods;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * Created by Dmitrii on 14.03.2018.
+ */
+public class CheckPathType {
+    public static void main(String[] args) {
+        Path path1 = Paths.get("C:\\birds\\eagle.jpg");
+        System.out.println("Is absolute: " + path1.isAbsolute());
+        System.out.println("To absolute: " + path1.toAbsolutePath());
+        // Is absolute: true
+        // To absolute: C:\birds\eagle.jpg
+
+        Path path2 = Paths.get("animals/birds/eagle.jpg");
+        System.out.println("Is absolute: " + path2.isAbsolute());
+        System.out.println("To absolute: " + path2.toAbsolutePath());
+        // Is absolute: false
+        // To absolute: C:\Workspace\JavaProfessional\animals\birds\eagle.jpg
+    }
+}
