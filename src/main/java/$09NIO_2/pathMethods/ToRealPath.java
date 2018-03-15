80db8abb5f5c87e8255204446e463d8ef8a56ee2
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2018-03-15 19:36:52 +0300

    toRealPath

---
+++ src/main/java/$09NIO_2/pathMethods/ToRealPath.java
@@ -0,0 +1,24 @@
+package $09NIO_2.pathMethods;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * Created by Dmitrii on 15.03.2018.
+ */
+public class ToRealPath {
+    public static void main(String[] args) {
+        try {
+            Path file = Paths.get("src\\main\\resources\\animal.data");
+            System.out.println(file.toRealPath());
+            // C:\Workspace\JavaProfessional\src\main\resources\animal.data
+
+            Path notExist = Paths.get("some/strange/path.file");
+            System.out.println(notExist.toRealPath());
+        } catch (IOException e) {
+            System.out.println("File doesn't exist: " + e.getMessage());
+            // File doesn't exist: C:\Workspace\JavaProfessional\some\strange\path.file
+        }
+    }
+}
