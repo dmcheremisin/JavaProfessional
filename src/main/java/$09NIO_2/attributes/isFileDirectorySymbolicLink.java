4f46ee3ce72d53ac7227c7e8236d7a99bcdbd11f
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2018-03-19 19:52:48 +0300

    main attributes

---
+++ src/main/java/$09NIO_2/attributes/isFileDirectorySymbolicLink.java
@@ -0,0 +1,23 @@
+package $09NIO_2.attributes;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * Created by Dmitrii on 19.03.2018.
+ */
+public class isFileDirectorySymbolicLink {
+    public static void main(String[] args) {
+        Path resources = Paths.get("src\\main\\resources\\");
+        Path dir = resources.resolve("dir");
+        Path file = resources.resolve("sample.txt");
+        Path link = resources.resolve("C:\\files\\linkToFile");
+
+        System.out.println(Files.isDirectory(dir));// true
+        System.out.println(Files.isRegularFile(file));// true
+        System.out.println(Files.isSymbolicLink(link));// true
+        System.out.println(Files.isRegularFile(link));// true
+
+    }
+}
