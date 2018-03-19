c8e7ef02a20e0ae1396ca33eb84d6df820069160
dmitrii89 <1Lovemoney>
2018-03-19 20:28:23 +0300

    isHidden

--- src/main/java/$09NIO_2/attributes/isFileDirectorySymbolicLinkHidden.java
+++ src/main/java/$09NIO_2/attributes/isFileDirectorySymbolicLinkHidden.java
@@ -1,5 +1,6 @@
 package $09NIO_2.attributes;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -7,16 +8,20 @@ import java.nio.file.Paths;
 /**
  * Created by Dmitrii on 19.03.2018.
  */
-public class isFileDirectorySymbolicLink {
+public class isFileDirectorySymbolicLinkHidden {
     public static void main(String[] args) {
-        Path resources = Paths.get("src\\main\\resources\\");
-        Path file = resources.resolve("sample.txt");
+        Path res = Paths.get("src\\main\\resources\\");
+        Path file = res.resolve("sample.txt");
         Path link = Paths.get("C:\\files\\linkToFile");
 
-        System.out.println(Files.isDirectory(resources));// true
+        System.out.println(Files.isDirectory(res));// true
         System.out.println(Files.isRegularFile(file));// true
         System.out.println(Files.isSymbolicLink(link));// true
         System.out.println(Files.isRegularFile(link));// true
+        try {
+            System.out.println(Files.isHidden(file)); // true
+            System.out.println(Files.isHidden(res.resolve("sample_copy.txt"))); // false
+        } catch (IOException e) { }
 
     }
 }
