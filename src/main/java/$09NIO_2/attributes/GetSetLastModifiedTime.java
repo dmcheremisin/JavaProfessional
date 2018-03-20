42ff04f7b9f02053271a70fe238313c4b5361843
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2018-03-20 14:18:36 +0300

    Get set lastModified time

---
+++ src/main/java/$09NIO_2/attributes/GetSetLastModifiedTime.java
@@ -0,0 +1,24 @@
+package $09NIO_2.attributes;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.FileTime;
+
+/**
+ * Created by Dmitrii on 20.03.2018.
+ */
+public class GetSetLastModifiedTime {
+    public static void main(String[] args) {
+        Path file = Paths.get("src\\main\\resources\\sample.txt");
+        try {
+            FileTime fileTime = Files.getLastModifiedTime(file);
+            System.out.println(fileTime); // 2018-03-05T12:11:27.938543Z
+            Files.setLastModifiedTime(file, FileTime.fromMillis(System.currentTimeMillis()));
+            System.out.println(Files.getLastModifiedTime(file)); // 2018-03-20T11:18:07.142Z
+        } catch (IOException e) {
+        }
+
+    }
+}
