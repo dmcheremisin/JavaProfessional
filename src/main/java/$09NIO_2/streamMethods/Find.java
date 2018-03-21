07997ecbc066e08fef0c594bcbf6b214bb65fa8f
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2018-03-21 20:56:31 +0300

    Find

---
+++ src/main/java/$09NIO_2/streamMethods/Find.java
@@ -0,0 +1,25 @@
+package $09NIO_2.streamMethods;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
+
+/**
+ * Created by Dmitrii on 21.03.2018.
+ */
+public class Find {
+    public static void main(String[] args) throws IOException {
+        Path file = Paths.get("src\\main\\resources\\sample.txt");
+        Path path = Paths.get("src\\main\\resources\\");
+        BasicFileAttributes data = Files.readAttributes(file, BasicFileAttributes.class);
+        FileTime sampleTime = data.creationTime();
+
+        Files.find(path, 10, (p,a) -> p.toString().contains("txt") && a.creationTime().equals(sampleTime))
+                .forEach(System.out::println);
+        // src\main\resources\sample.txt
+
+    }
+}
