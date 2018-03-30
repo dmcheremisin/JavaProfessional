26e239e5447e886ac47ba139a6a9de908075f911
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2018-03-30 14:46:36 +0300

    email regexp example

---
+++ src/main/java/$05DatesStringsLocalization/regexp/EmailRegexp.java
@@ -0,0 +1,23 @@
+package $05DatesStringsLocalization.regexp;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by Dmitrii on 30.03.2018.
+ */
+public class EmailRegexp {
+    public static void main(String[] args) {
+        String str = "Danny Doo, Flat no 502, Big Apartment, Wide Road, Near Huge Milestone, " +
+                "Hugo-city 56010, Ph: 9876543210, Email: danny@myworld.com. Maggi Myer, Post bag no 52, Big bank post" +
+                "office, Big bank city 56000, ph: 9876501234, Email: maggi07@myuniverse.com.";
+
+        Pattern pattern = Pattern.compile("\\b\\w+@\\w+\\.\\w+\\b");
+        Matcher matcher = pattern.matcher(str);
+        while(matcher.find()){
+            System.out.println(matcher.group());
+        }
+        // danny@myworld.com
+        // maggi07@myuniverse.com
+    }
+}
