ef8aae0ea4813ac50c4eeeba39fec5c7f750c994
dmitrii89 <1Lovemoney>
2018-04-05 20:34:05 +0300

    Starvation

---
+++ src/main/java/$07Concurrency/threadStates/Starvation.java
@@ -0,0 +1,33 @@
+package $07Concurrency.threadStates;
+
+import $07Concurrency.Utils;
+
+public class Starvation {
+
+    public static void main(String[] args) {
+        System.out.println("start");
+
+        Object pager = new Object();
+        
+        new Thread(() -> {
+            synchronized (pager) { // locks pager
+                try {
+                    pager.wait(); // frees pager and waits for the signal from the main thread
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println("Huuuray!");
+        }).start();
+
+        Utils.pause(2000);
+        
+        synchronized (pager){ // locks pager
+            pager.notify(); // notifies that pager is free
+            while (true){} // but doesn't release lock here -> main thread is RUNNING, new Thread is in MONITOR state
+        }
+        // start
+        // ...waiting forever - starvation
+    }
+
+}
