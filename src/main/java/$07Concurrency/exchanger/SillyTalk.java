7321ed70bae5c4d139e3de2b64e08b3d5d6db179
dmch0916 <dmitry_cher@mail.ru>
2018-04-26 17:09:49 +0300

    Exchanger example

---
+++ src/main/java/$07Concurrency/exchanger/SillyTalk.java
@@ -0,0 +1,52 @@
+package $07Concurrency.exchanger;
+
+import java.util.concurrent.Exchanger;
+
+class Duke extends Thread {
+    Exchanger<String> message;
+
+    public Duke(Exchanger<String> message){
+        this.message = message;
+    }
+    public void run(){
+        String reply = null;
+        try {
+            reply = message.exchange("Knock knock!");
+            System.out.println("CoffeeShop: " + reply);
+            reply = message.exchange("Duke");
+            System.out.println("CoffeeShop: " + reply);
+            reply = message.exchange("The one who was born in this coffee shop!");
+        } catch(InterruptedException ie) {
+            System.err.println("Got interrupted during my silly talk");
+        }
+    }
+}
+
+class CoffeeShop extends Thread {
+    Exchanger<String> message;
+
+    public CoffeeShop(Exchanger<String> message){
+        this.message = message;
+    }
+    public void run(){
+        String reply = null;
+        try {
+            reply = message.exchange("Who's there?");
+            System.out.println("Duke: " + reply);
+            reply = message.exchange("Duke who?");
+            System.out.println("Duke: " + reply);
+            reply = message.exchange("");
+            System.out.println("Duke: " + reply);
+        } catch(InterruptedException ie) {
+            System.err.println("Got interrupted during my silly talk");
+        }
+    }
+}
+
+public class SillyTalk {
+    public static void main(String[] args) {
+        Exchanger<String> message = new Exchanger<>();
+        new Duke(message).start();
+        new CoffeeShop(message).start();
+    }
+}
