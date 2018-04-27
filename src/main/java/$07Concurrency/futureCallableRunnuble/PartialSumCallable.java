7aa898b88669f4ef31283a1d0da3a74a3e323a9a
dmch0916 <dmitry_cher@mail.ru>
2018-04-27 21:04:33 +0300

    Future and Callable Sum task

---
+++ src/main/java/$07Concurrency/futureCallableRunnuble/PartialSumCallable.java
@@ -0,0 +1,51 @@
+package $07Concurrency.futureCallableRunnuble;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
+public class PartialSumCallable {
+    private static int THREADS = 10;
+    private static long NUM = 1_000_000_000L;
+    private static long sum = 0;
+
+    static class SumCallable implements Callable<Long> {
+        long from, to;
+
+        public SumCallable(long from, long to) {
+            this.from = from;
+            this.to = to;
+        }
+
+        @Override
+        public Long call() throws Exception {
+            long result = 0L;
+            for(long i = from; i <= to; i++){
+                result += i;
+            }
+            return result;
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+        List<Future<Long>> futures = new ArrayList<>();
+        ExecutorService service = Executors.newFixedThreadPool(THREADS);
+        Long part = NUM / THREADS;
+        int from = 0;
+        int to = 0;
+        for(int i = 0; i < THREADS; i++){
+            from = to;
+            to += part;
+            SumCallable sumCallable = new SumCallable(from, to);
+            Future<Long> future = service.submit(sumCallable);
+            futures.add(future);
+        }
+
+        for (Future<Long> future : futures) {
+            sum += future.get();
+        }
+        System.out.println("The sum = " + sum);
+        service.shutdown();
+    }
+
+}
