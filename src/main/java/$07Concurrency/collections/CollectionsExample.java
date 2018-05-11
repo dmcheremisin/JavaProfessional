975c7faab074b45bf5d30552c6fbf7e75ee6ca01
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2018-05-11 14:54:55 +0300

    Concurrent collections example

---
+++ src/main/java/$07Concurrency/collections/CollectionsExample.java
@@ -0,0 +1,38 @@
+package $07Concurrency.collections;
+
+import java.util.Deque;
+import java.util.Map;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.LinkedBlockingQueue;
+
+/**
+ * Created by Dmitrii on 11.05.2018.
+ */
+public class CollectionsExample {
+    public static void main(String[] args) {
+        Map<String, Integer> map = new ConcurrentHashMap<>();
+        map.put("elephant", 10);
+        System.out.println(map.get("elephant"));
+
+        Queue<String> blockingQueue = new LinkedBlockingQueue<>();
+        blockingQueue.offer("one");
+        blockingQueue.add("two");
+        System.out.println(blockingQueue); // [one, two]
+        System.out.println(blockingQueue.peek()); // one
+        System.out.println(blockingQueue.poll()); // one
+        System.out.println(blockingQueue); // [two]
+
+        Deque<String> blockingDeque = new LinkedBlockingDeque<>();
+        blockingDeque.offer("one");
+        blockingDeque.add("two");
+        blockingDeque.push("three");
+        System.out.println(blockingDeque); // [three, one, two]
+        System.out.println(blockingDeque.peek()); // three
+        System.out.println(blockingDeque.poll()); // three
+        System.out.println(blockingDeque); // [one, two]
+        
+        
+    }
+}
