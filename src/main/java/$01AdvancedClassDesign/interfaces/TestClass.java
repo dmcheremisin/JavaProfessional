d75387ed86263d553d70df7ddf3cb832a62f94ac
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2018-05-21 20:30:55 +0300

    interfaces access to methods

---
+++ src/main/java/$01AdvancedClassDesign/interfaces/TestClass.java
@@ -0,0 +1,30 @@
+package $01AdvancedClassDesign.interfaces;
+
+/**
+ * Created by Dmitrii on 21.05.2018.
+ */
+interface House {
+    public default String getAddress() {
+        return "101 Main Str";
+    }
+}
+interface Office {
+    public static String getAddress() {
+        return "101 Smart Str";
+    }
+}
+// no problem that one interface has static method and another default
+interface WFH extends House, Office {}
+
+class HomeOffice implements House, Office {
+    public String getAddress() {
+        return "R No 1, Home";
+    }
+}
+public class TestClass {
+    public static void main(String[] args) {
+        Office off = new HomeOffice();
+        System.out.println(Office.getAddress());// static method should be called only with interface reference
+        // 101 Smart Str
+    }
+}
