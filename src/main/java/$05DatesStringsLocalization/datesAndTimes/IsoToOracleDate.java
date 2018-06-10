0517f3de8d17973d3bc53e6b3871fcf8be51f98b
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2018-06-10 14:09:05 +0300

    Zoned date time

--- src/main/java/$05DatesStringsLocalization/datesAndTimes/IsoToOracleDate.java
+++ src/main/java/$05DatesStringsLocalization/datesAndTimes/IsoToOracleDate.java
@@ -17,12 +17,14 @@ public class IsoToOracleDate {
         String zone = timezones.get("NLD");
         ZoneId zoneId = ZoneId.of(zone);
         long epochMilli = 1525370235 * 1000L;
-        LocalDateTime dt = Instant.ofEpochMilli(epochMilli).atZone(zoneId).toLocalDateTime();
-        System.out.println(dt);
+        ZonedDateTime zonedDateTime = Instant.ofEpochMilli(epochMilli).atZone(zoneId);
+        System.out.println(zonedDateTime); // 2018-05-03T19:57:15+02:00[Europe/Amsterdam]
+
+        LocalDateTime dt = zonedDateTime.toLocalDateTime();
+        System.out.println(dt); // 2018-05-03T19:57:15
+
         ZonedDateTime zdt = ZonedDateTime.of(dt, zoneId);
-        System.out.println(zdt.format(DateTimeFormatter.ISO_OFFSET_DATE_TIME));
-        // 2018-05-03T19:57:15
-        // 2018-05-03T19:57:15+02:00
+        System.out.println(zdt.format(DateTimeFormatter.ISO_OFFSET_DATE_TIME)); // 2018-05-03T19:57:15+02:00
     }
 
 }
