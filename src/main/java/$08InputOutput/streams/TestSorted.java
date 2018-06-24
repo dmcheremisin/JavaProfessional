0db3aac3c3ccc66a7cf5f7ca83fb90986f019768
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2018-06-24 18:34:10 +0300

    Test sorted in runtime

---
+++ src/main/java/$08InputOutput/streams/TestSorted.java
@@ -0,0 +1,28 @@
+package $08InputOutput.streams;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Dmitrii on 24.06.2018.
+ */
+public class TestSorted {
+    public static class Book {
+        String isbn;
+        String title;
+
+        public Book(String isbn, String title) {
+            this.isbn = isbn;
+            this.title = title;
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Book> books = new ArrayList<>();
+        books.add(new Book("123", "Big Bang"));
+        books.add(new Book("233", "New"));
+        books.stream().sorted().forEach(b->System.out.println(b.isbn)); // ClassCastException
+        // cast to comparable happens in runtime
+        // Exception in thread "main" java.lang.ClassCastException: $08InputOutput.streams.TestSorted$Book cannot be cast to java.lang.Comparable
+    }
+}
