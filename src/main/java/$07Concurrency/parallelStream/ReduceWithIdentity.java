8f97996c035c879414cb48c468c6a42d5b715e6c
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2018-06-25 14:40:32 +0300

    Parallel vs sequential reduction

--- src/main/java/$07Concurrency/parallelStream/ReduceWithIdentity.java
+++ src/main/java/$07Concurrency/parallelStream/ReduceWithIdentity.java
@@ -10,16 +10,15 @@ public class ReduceWithIdentity {
     public static void main(String[] args) {
         List<String> vals = Arrays.asList("a", "b", "c", "d", "e", "f", "g");
         String join = vals.parallelStream()
-                .peek(System.out::println)
-                .reduce("_", String::concat, (a, b) -> a.concat(b));
+                .reduce("_", String::concat);
         System.out.println("Result: " + join);
-        //a
-        //c
-        //b
-        //e
-        //f
-        //g
-        //d
         // Result: _a_b_c_d_e_f_g
+
+        join = vals.stream()
+                .reduce("_", String::concat);
+        System.out.println("Result: " + join);
+        // Result: _abcdefg
+
+
     }
 }
